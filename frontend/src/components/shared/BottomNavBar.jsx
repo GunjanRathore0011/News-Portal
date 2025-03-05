91e13d15d358d31cbc49c8e6929a81154699ee0c
Gunjan Rathore <rathoregunjan46@gmail.com>
2025-03-05 19:05:07 +0530

    add post section to the bottom bar

--- frontend/src/components/shared/BottomNavBar.jsx
+++ frontend/src/components/shared/BottomNavBar.jsx
@@ -1,14 +1,16 @@
 import { useToast } from '@/hooks/use-toast'
 import { signOutSuccess } from '@/redux/user/userSlice'
+import { current } from '@reduxjs/toolkit'
 import React, { useDebugValue } from 'react'
 import { FaHome, FaSignOutAlt, FaUserAlt } from 'react-icons/fa'
-import { useDispatch } from 'react-redux'
+import { IoIosCreate, IoIosDocument } from 'react-icons/io'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const BottomNavBar = () => {
-    const dispatch=useDispatch();
-    const {toast}=useToast();
-
+    const dispatch = useDispatch();
+    const { toast } = useToast();
+    const { currentUser } = useSelector((state) => state.user);
     const signOutHandler = async () => {
         try {
             const res = await fetch("/api/user/signOut", {
@@ -33,19 +35,30 @@ const BottomNavBar = () => {
     return (
         <nav className='md:hidden fixed bottom-0 left-0 right-0 bg-slate-200 border-t border-gray-300 p-2 flex justify-around items-center'>
 
-            <Link to='/' className='flex flex-col items-center text-slate-800' >
-                <FaHome size={20}></FaHome>
-                <span className='text-xs'>Home</span>
-            </Link>
-
             <Link to='/dashboard?tab=profile' className='flex flex-col items-center text-slate-800' >
                 <FaUserAlt size={20}></FaUserAlt>
                 <span className='text-sm' >Profile</span>
             </Link>
 
-            <button className='flex flex-col items-center text-slate-800'onClick={signOutHandler}  >
+            {currentUser && currentUser.isAdmin && (
+                <Link to='/create-post' className='flex flex-col items-center text-slate-800' >
+                    <IoIosCreate size={20}></IoIosCreate>
+                    <span className='text-sm' >Create Post</span>
+                </Link>
+            )
+            }
+
+            {currentUser && currentUser.isAdmin && (
+                <Link to='/dashboard?tab=posts' className='flex flex-col items-center text-slate-800' >
+                    <IoIosDocument size={20}></IoIosDocument>
+                    <span className='text-sm' >Post</span>
+                </Link>
+            )
+            }
+
+            <button className='flex flex-col items-center text-slate-800' onClick={signOutHandler}  >
                 <FaSignOutAlt size={20}></FaSignOutAlt>
-                <span  className='text-sm'>Logout</span>
+                <span className='text-sm'>Logout</span>
             </button>
         </nav>
     )
